dc6a8a05067f94d74ad379afeb17cec0ea5c239c
임창현 <changhyun@finger-plus.com>
2018-05-09 00:24:06 +0900

    spring boot init journal project

---
+++ src/main/java/com/jonathan/springjournal/SpringjournalApplication.java
@@ -0,0 +1,12 @@
+package com.jonathan.springjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjournalApplication.class, args);
+    }
+}
